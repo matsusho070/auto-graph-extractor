22d3386a0a840c5a887894ec2aad3daf7a5bb11e
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2024-05-19 07:16:12 +0000

    Update main.py to use dynamic model name in main function

--- app/main.py
+++ app/main.py
@@ -14,14 +14,13 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 
-def main(use_cache, article_text):
+def main(use_cache, article_text, model_name):
     set_llm_cache(SQLiteCache(database_path=".langchain.db"))
 
     # Initialize Lang Smith
     client = Client()
 
-    # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7, cache=use_cache)
-    llm = ChatOpenAI(model_name="gpt-4-turbo-2024-04-09", temperature=0.7, cache=use_cache)
+    llm = ChatOpenAI(model_name=model_name, temperature=0.7, cache=use_cache)
 
     existing_event_list = ["地震", "台風", "洪水", "火事"]
     event_creation_tool = EventCreationTool()
@@ -38,9 +37,10 @@ def main(use_cache, article_text):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process workspace directory')
     parser.add_argument('--no_cache', action='store_true', help='Invalidate cache')
+    parser.add_argument('--model_name', type=str, default="gpt-4o", help='Model name')
     args = parser.parse_args()
     # Read from stdin
     article_text = input("Enter article text: ")
 
 
-    main(not args.no_cache, article_text)
+    main(not args.no_cache, article_text, args.model_name)
